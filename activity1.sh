c18bbbd30c29234fbc9c9ebf30024a89a357407c
John Keith Mercado <152822744+KeithMercado@users.noreply.github.com>
2025-02-05 01:42:14 +0000

    Final

---
+++ activity1.sh
@@ -0,0 +1,17 @@
+# Declare variables
+NAME="NAME"
+AGE="AGE"
+PROGRAM="PROGRAM"
+
+# User input
+echo "Enter Name:"
+read NAME
+
+echo "Enter Your Age:"
+read AGE
+
+echo "Enter Program:"
+read PROGRAM
+
+# Display message
+echo "Hello $NAME, your age is $AGE & your program is $PROGRAM."
